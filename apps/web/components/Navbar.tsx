d45af937422e985d252379a74bc468d203531da0
eightants <yihonganthony@gmail.com>
2023-07-26 17:50:09 -0400

    Added links, format eslint config

--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -4,10 +4,28 @@ import { buttonVariants } from 'ui'
 
 const Navbar = () => {
 	return (
-		<nav className="w-full bg-transparent flex justify-center pt-3 sticky top-0 z-10">
+		<nav className="w-full bg-transparent flex justify-center pt-3 sticky top-0 z-20">
 			<div className="flex border-border border rounded-full bg-card py-1.5 px-4 items-center gap-4 shadow-sm">
 				<img src={OdysseyIcon.src} className="w-4 h-4" />
-				<div className="flex gap-2">
+				<div className="flex gap-1">
+					<Link
+						href={'/tools'}
+						className={`${buttonVariants({
+							variant: 'ghost',
+						})} !h-auto !px-3 !py-1.5 m-[1px] !rounded-full !text-gray-500 !font-normal hover:text-gray-700 hover:border-border hover:border hover:m-0`}
+					>
+						Tiny Tools
+					</Link>
+					<Link
+						target="_blank"
+						rel="noopener nofollow noreferrer"
+						href={'https://videorecap.viewodyssey.com'}
+						className={`${buttonVariants({
+							variant: 'ghost',
+						})} !h-auto !px-3 !py-1.5 m-[1px] !rounded-full !text-gray-500 !font-normal hover:text-gray-700 hover:border-border hover:border hover:m-0`}
+					>
+						Video Recap
+					</Link>
 					<Link
 						target="_blank"
 						rel="noopener nofollow noreferrer"
