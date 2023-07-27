da5bc54940f0c995de087a1d885fd03094e43562
eightants <yihonganthony@gmail.com>
2023-07-26 20:05:39 -0400

    Use common eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	plugins: ['react', '@typescript-eslint'],
-	rules: {
-		'react/react-in-jsx-scope': 'off',
-	},
+	extends: ['custom'],
 }
--- apps/webstore-rank/.eslintrc.js
+++ apps/webstore-rank/.eslintrc.js
@@ -1,26 +1,4 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  root: true,
-  extends: ["custom"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["@typescript-eslint", "react"],
-  rules: {},
-};
+	root: true,
+	extends: ['custom'],
+}
