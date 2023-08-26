1d5304e10c531d37392ca0b2d4f6781142d4f696
eightants <yihonganthony@gmail.com>
2023-08-26 17:17:59 +0800

    Add highlght on web

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import Script from 'next/script'
 import { Toaster } from 'ui'
 import 'ui/styles.css'
 import Wrapper from '../components/Wrapper'
+import { HighlightInit } from '@highlight-run/next/highlight-init'
 
 const headData = {
 	title: 'Odyssey - View your digital journey. ',
@@ -22,27 +23,42 @@ export default function RootLayout({
 	children: React.ReactNode
 }) {
 	return (
-		<html lang="en">
-			<Script
-				id="gaScript1"
-				strategy="afterInteractive"
-				src="https://www.googletagmanager.com/gtag/js?id=G-KFTQWN9V6D"
-			></Script>
-			<Script
-				id="gaScript2"
-				strategy="afterInteractive"
-				dangerouslySetInnerHTML={{
-					__html: `window.dataLayer = window.dataLayer || [];
+		<>
+			{process.env['NODE_ENV'] !== 'development' ? (
+				<HighlightInit
+					projectId={'ng2kkyg1'}
+					tracingOrigins
+					networkRecording={{
+						enabled: true,
+						recordHeadersAndBody: true,
+						urlBlocklist: [],
+					}}
+				/>
+			) : (
+				<></>
+			)}
+			<html lang="en">
+				<Script
+					id="gaScript1"
+					strategy="afterInteractive"
+					src="https://www.googletagmanager.com/gtag/js?id=G-KFTQWN9V6D"
+				></Script>
+				<Script
+					id="gaScript2"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-KFTQWN9V6D');`,
-				}}
-			/>
-			<body>
-				<Wrapper>{children}</Wrapper>
-				<Toaster />
-			</body>
-		</html>
+					}}
+				/>
+				<body>
+					<Wrapper>{children}</Wrapper>
+					<Toaster />
+				</body>
+			</html>
+		</>
 	)
 }
