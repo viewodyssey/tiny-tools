a83323a322ff088e15b66765c6df8095f989df30
eightants <yihonganthony@gmail.com>
2023-08-31 13:31:27 +0800

    Change top left url

--- packages/ui/components/Sidebar/Sidebar.tsx
+++ packages/ui/components/Sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ interface SidebarProps {
 export const Sidebar = ({ children, sidebarUrl }: SidebarProps) => {
 	return (
 		<div className="w-[240px] basis-[240px] flex-shrink-0 flex-grow-0 bg-background border-r border-border border-solid py-4 px-4">
-			<a href={sidebarUrl || 'https://viewodyssey.com/tools'}>
+			<a href={sidebarUrl || 'https://viewodyssey.com'}>
 				<div className="w-full max-w-[150px] pl-2">
 					<TinyToolsWordmark />
 				</div>
