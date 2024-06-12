b953dbf23cc2d62fdab99b7a3c1470ba18c9faf6
eightants <yihonganthony@gmail.com>
2024-06-11 20:18:23 -0400

    redirect

--- apps/moist/next.config.js
+++ apps/moist/next.config.js
@@ -3,4 +3,14 @@ module.exports = {
 	transpilePackages: ['ui'],
 	trailingSlash: false,
 	basePath: '/moist-meter',
+	async redirects() {
+		return [
+			{
+				source: '/get',
+				destination:
+					'https://chromewebstore.google.com/detail/moist-meter/fkjclkbcfieknijloglaflnkdekgccna',
+				permanent: true,
+			},
+		]
+	},
 }
