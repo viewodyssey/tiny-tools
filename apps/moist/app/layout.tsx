5866bfd750d43d42ac2dc7065fb57fe0abe445b5
eightants <yihonganthony@gmail.com>
2024-06-11 20:13:59 -0400

    fix og

--- apps/moist/app/layout.tsx
+++ apps/moist/app/layout.tsx
@@ -17,6 +17,7 @@ export const metadata: Metadata = {
 	title: headData.title,
 	description: headData.description,
 	openGraph: { title: headData.title, description: headData.description },
+	metadataBase: new URL('https://viewodyssey.com/moist-meter/'),
 }
 
 export default function RootLayout({
